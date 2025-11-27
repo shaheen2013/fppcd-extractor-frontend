245881e0fbb50ccf512116cd00f23730d2a3020e
Sadman Sakib <ssakib4040@gmail.com>
2025-11-27 14:10:08 +0600

    Simplifies homepage structure for improved maintainability and visual consistency.

--- app/(homepage)/layout.tsx
+++ app/(homepage)/layout.tsx
@@ -18,11 +18,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-        <Header />
-        {children}
-      </body>
-    </html>
+    <div className="">
+      <Header />
+      {children}
+    </div>
   );
 }
--- app/(homepage)/page.tsx
+++ app/(homepage)/page.tsx
@@ -1,3 +1,37 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>homepage</div>;
+  return (
+    <div className="">
+      <div
+        className="relative py-24 px-4 sm:px-8 flex flex-col items-center justify-center text-center"
+        style={{
+          background:
+            "linear-gradient(135deg, #1e40af 0%, #3b82f6 50%, #06b6d4 100%)",
+        }}
+      >
+        <h1 className="text-3xl md:text-4xl font-bold text-white mb-6 drop-shadow-lg">
+          Select Planning Conditions
+        </h1>
+        <p className="text-lg sm:text-xl text-blue-100  max-w-4xl">
+          Choose the planning conditions you want to search for in UK planning
+          applications. Our system will find all matching applications with
+          detailed information.
+        </p>
+      </div>
+
+      <div className="">
+        <div className="container my-16 mx-auto grid grid-cols-2 gap-8">
+          {/* item */}
+          <Link
+            href="/report?data=1"
+            className="bg-white p-6 rounded-xl shadow-[0px_4px_6px_2px_rgba(0,0,0,0.1)] flex justify-between"
+          >
+            <div>icon</div>
+            <div>Heritage & Listed Buildings</div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
